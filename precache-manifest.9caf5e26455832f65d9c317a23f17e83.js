97463443a952572f10c5c092d4843897c141c5e3
DLRoqueSantos <diogosantostm@gmail.com>
2023-09-17 16:50:57 -0400

    Updates

---
+++ precache-manifest.9caf5e26455832f65d9c317a23f17e83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce95ba4b9f110130496a",
+    "url": "/todoapp/static/css/main.a14ed793.chunk.css"
+  },
+  {
+    "revision": "ce95ba4b9f110130496a",
+    "url": "/todoapp/static/js/main.ce95ba4b.chunk.js"
+  },
+  {
+    "revision": "937ddaef85b2fc67bdac",
+    "url": "/todoapp/static/js/1.937ddaef.chunk.js"
+  },
+  {
+    "revision": "ca8789d6bb6dac684f74",
+    "url": "/todoapp/static/js/runtime~main.ca8789d6.js"
+  },
+  {
+    "revision": "1b63c05b47673c808ea56bbdf8121d35",
+    "url": "/todoapp/index.html"
+  }
+];
